4ff88b4ade5b1c760e9f438e138d23d73d3df124
Elizabeth Munoz <munoz_714@live.com>
2020-05-08 21:20:42 -0700

    updateOne orm function setup

--- config/orm.js
+++ config/orm.js
@@ -39,6 +39,24 @@ var orm = {
             cb(result);
         });
     },
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += 'devoured = true'
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            // console.log("orm", result);
+            cb(result);
+        });
+    },
+
 
 }
 
