5bfd0e60cef2bbd9b38cec03c12dd503109c7272
Elizabeth Munoz <munoz_714@live.com>
2020-05-09 22:41:01 -0700

    test error

--- config/orm.js
+++ config/orm.js
@@ -14,7 +14,6 @@ var orm = {
     selectAll: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, [tableInput], function (err, result) {
-            if (err) { throw err; };
             // console.log("orm", result);
             cb(result)
         });
