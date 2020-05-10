e7f158779615574a3beed3ba699701a69574fabf
Elizabeth Munoz <munoz_714@live.com>
2020-05-09 21:51:05 -0700

    deploy heroku

--- config/orm.js
+++ config/orm.js
@@ -14,7 +14,7 @@ var orm = {
     selectAll: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, [tableInput], function (err, result) {
-            if (err) throw err;
+            if (err) { throw err; };
             // console.log("orm", result);
             cb(result)
         });
