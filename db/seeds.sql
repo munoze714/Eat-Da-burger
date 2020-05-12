c6a5ab2737a951ff2d7a5f8ea779173e60e209f7
Elizabeth Munoz <munoz_714@live.com>
2020-05-11 17:58:05 -0700

    server.js for main in package.json

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name,devoured) VALUES ('Big Mac', false);
-INSERT INTO burgers (burger_name,devoured) VALUES ('Western'true);
-INSERT INTO burgers (burger_name,devoured) VALUES ('bacon'false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Western',true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('bacon',false);
